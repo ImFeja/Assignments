8947d45e0b6e23a65fadbb73dd927016fe966ab0
ottd6393 <ottd6393@HRH-IC0027330.wrdsb.ca>
2018-09-24 12:41:25 -0400

    Assignment A2Q1 A2Q2 A2Q3

---
+++ src/assignment/pkg2/A2Q3.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment.pkg2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ottd6393
+ */
+public class A2Q3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please input a number");
+        int Numba = input.nextInt();
+        double equal=Numba%2;
+        if(equal==0.0){
+            System.out.println(+Numba+" is an equal number.");
+        }
+        if(equal==1){
+            System.out.println(+Numba+" is not an equal number.");
+        }
+        if(equal==0.5){
+            System.out.println(+Numba+" is not an equal number.");
+        }
+    }
+}
