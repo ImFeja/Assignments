09478a6564c9301d1016ef2cf9b272655463c6e7
ottd6393 <ottd6393@HRH-IC0027330.wrdsb.ca>
2018-09-24 12:58:16 -0400

    Assignment A2Q1 A2Q2 A2Q3 A1Q4(Temp)

---
+++ src/assignment/pkg2/A2Q4.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment.pkg2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ottd6393
+ */
+public class A2Q4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter a number.");
+        int Numba = input.nextInt();
+        if(Numba%11==0.0){
+            System.out.println(+ Numba + " is a special number!");   
+        }
+        if(Numba%11==1.0){
+            System.out.println(+ Numba + " is a special number!");   
+        }
+        
+        
+    }
+}
+
