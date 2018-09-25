afb79b6acb78cca047b5e6a9c907ae8c0b8ed7d4
ottd6393 <ottd6393@HRH-IC0027330.wrdsb.ca>
2018-09-25 13:19:30 -0400

    Assignment A2Q1 A2Q2 A2Q3 A2Q4(TEMP) A2Q5 A2Q6

---
+++ src/assignment2/pkg2/A2Q6.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment2.pkg2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ottd6393
+ */
+public class A2Q6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("What day of the week is tomorrow? (1=Monday...7=Sunday.)");
+        int Day = input.nextInt();
+        System.out.println("Is it a holiday? (0=No, 1=Yes)");
+        int Holiday = input.nextInt();
+        if(Day<=5){
+            if(Holiday==1){
+                System.out.println("You should set an alarm for 10:00 AM.");
+            }else{
+                System.out.println("You should set your alarm for 7:00 Am.");
+            }
+        }else{
+            
+        }
+    }
+}
