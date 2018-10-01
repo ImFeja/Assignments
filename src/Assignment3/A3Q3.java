d0d660946c32caa44519977b6b307ce4fa7db945
ottd6393 <ottd6393@HRH-IC0027330.wrdsb.ca>
2018-10-01 12:54:51 -0400

    Assignment A2Q1 A2Q2 A2Q3 A2Q4(TEMP) A2Q5 A2Q6 A2Q7 A2Q8 A2Q9 Assignment A3Q1 A3Q2(NO WORK) A3Q3

---
+++ src/Assignment3/A3Q3.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ottd6393
+ */
+public class A3Q3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+                System.out.println("Please enter a number between 1 and 10.");
+                double Numba = input.nextInt();
+                while(1>Numba || Numba>10){
+                    System.out.println("Between 1 and 10.");
+                    Numba = input.nextInt();
+                }
+                System.out.println("You chose"+Numba);
+    }
+}
