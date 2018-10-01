04374199053f9557ff9698717ad15c058f8c52c4
ottd6393 <ottd6393@HRH-IC0027330.wrdsb.ca>
2018-10-01 12:41:03 -0400

    Assignment A2Q1 A2Q2 A2Q3 A2Q4(TEMP) A2Q5 A2Q6 A2Q7 A2Q8 A2Q9 Assignment A3Q1 A3Q2(NO WORK)

---
+++ src/Assignment3/A3Q2.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ottd6393
+ */
+public class A3Q2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Scanner input = new Scanner(System.in);
+                System.out.println("Please enter a base.");
+                double Base = input.nextInt();
+                System.out.println("Please enter a exponent.");
+                double Exp = input.nextInt();
+                double Value=0;
+                double Times=0;
+                double Base2=Base;
+                while(Times<=Exp){
+                    Value=Base*Base2;
+                    Times=Times++;
+                }
+                System.out.println(+Value+".");
+    }
+}
