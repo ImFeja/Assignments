ae430d70b07ba273b3108141fb7667aec9cfaeaf
ottd6393 <ottd6393@HRH-IC0027330.wrdsb.ca>
2018-10-01 13:14:31 -0400

    Assignment A2Q1 A2Q2 A2Q3 A2Q4(TEMP) A2Q5 A2Q6 A2Q7 A2Q8 A2Q9 Assignment A3Q1 A3Q2(NO WORK) A3Q3 A3Q4 Start

---
+++ src/Assignment3/A3Q4.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ottd6393
+ */
+public class A3Q4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         Scanner input = new Scanner(System.in);
+                System.out.println("Please give us a number.");
+                double Numba = input.nextInt();
+    }
+}
