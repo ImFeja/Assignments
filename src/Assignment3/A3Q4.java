d4b0658258a6f9ce798f2e72ca534c4da1970d2a
ottd6393 <ottd6393@HRH-IC0027330.wrdsb.ca>
2018-10-02 12:50:50 -0400

    Assignment A2Q1 A2Q2 A2Q3 A2Q4(TEMP) A2Q5 A2Q6 A2Q7 A2Q8 A2Q9 Assignment A3Q1 A3Q2 A3Q3 A3Q4 Start

--- src/Assignment3/A3Q4.java
+++ src/Assignment3/A3Q4.java
@@ -18,6 +18,17 @@ public class A3Q4 {
     public static void main(String[] args) {
          Scanner input = new Scanner(System.in);
                 System.out.println("Please give us a number greater than 1.");
-                double Numba = input.nextInt();
+                int Numba = input.nextInt();
+                int counter=0;
+                int down = 1;
+                while(Numba<=0){                   
+                    System.out.println("Greater than 1 please.");
+                    Numba = input.nextInt();
+                    counter=counter+1;
+                    if(counter==5){
+                    System.out.println("JUST PUT A NUMBER BIGGER THAN ONE ALREADY!!!");
+                }
+                }  
+                
     }
 }
